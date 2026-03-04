54230b759084656c49c0c88ae1926978eecdb0a7
Makhintosh <igoruszat@gmail.com>
2026-03-04 17:49:52 +0100

    w pani mieszkaniu jest bomba

--- Ćwiczenia 2/Zad2/main.c
+++ Ćwiczenia 2/Zad2/main.c
@@ -49,32 +49,32 @@ int main(void)
 //    }
 
 //    Zad 1
-//    Napisz program, kt�ry oblicza reszt�
-//    z dzielenia sumy dw�ch liczb ca�kowitych przez trzeci� liczb� ca�kowit�.
+//    Napisz program, który oblicza resztê
+//    z dzielenia sumy dwóch liczb ca³kowitych przez trzeci¹ liczbê ca³kowit¹.
 
 //    int a = 8, b = 2, c = 3;
 //    int wynik = (a + b) % c;
 //    printf("%d", wynik);
 
 //    Zad 2
-//    Stw�rz program, kt�ry oblicza r�nic�
-//    kwadrat�w dw�ch podanych liczb ca�kowitych.
+//    Stwórz program, który oblicza ró¿nicê
+//    kwadratów dwóch podanych liczb ca³kowitych.
 
 //    int a = 4, b = 3;
 //    int wynik = (a*a) - (b*b);
 //    printf("%d", wynik);
 
 //    Zad 3
-//    Opracuj program, kt�ry oblicza iloczyn r�nicy dw�ch
-//    liczb ca�kowitych i trzeciej liczby ca�kowitej.
+//    Opracuj program, który oblicza iloczyn ró¿nicy dwóch
+//    liczb ca³kowitych i trzeciej liczby ca³kowitej.
 
 //    int a = 1, b = 2, c = 3;
 //    int wynik = (a - b) * c;
 //    printf("%d", wynik);
 
 //    Zad 4
-//    Zaprojektuj program, kt�ry oblicza �redni� geometryczn�
-//    bezwzgl�dnych warto�ci trzech podanych liczb ca�kowitych.
+//    Zaprojektuj program, który oblicza œredni¹ geometryczn¹
+//    bezwzglêdnych wartoœci trzech podanych liczb ca³kowitych.
 
 //    int a = 2, b = 3, c = -4;
 //    a = abs(a);
@@ -84,20 +84,140 @@ int main(void)
 //    printf("%.3lf", wynik);
 
 //    Zad 5
-//    Napisz program, kt�ry oblicza kwadrat
-//    sumy dw�ch podanych liczb ca�kowitych.
+//    Napisz program, który oblicza kwadrat
+//    sumy dwóch podanych liczb ca³kowitych.
 
 //    int a = 2, b = 3;
 //    int wynik = pow((a+b), 2);
 //    printf("%d", wynik);
 
 //    Zad 6
-//    Stw�rz program, kt�ry oblicza sum�
-//    kwadrat�w trzech podanych liczb ca�kowitych.
+//    Stwórz program, który oblicza sumê
+//    kwadratów trzech podanych liczb ca³kowitych.
 
 //    int a = 2, b = 3, c = 4;
 //    int wynik = (a*a) + (b*b) + (c*c);
 //    printf("%d", wynik);
+
+//    Zad 7
+//    Opracuj program, który oblicza, ile razy jedna podana liczba całkowita mieści się w drugiej podanej liczbie całkowitej.
+
+//    int a, b;
+//
+//    printf("Podaj pierwsza liczbe: ");
+//    scanf("%d", &a);
+//
+//    printf("Podaj druga liczbe: ");
+//    scanf("%d", &b);
+//
+//    int wynik = b / a;
+//    printf("Miesci sie %d razy", wynik);
+
+//    Zad 8
+//    Zaprojektuj program, który oblicza kwadrat różnicy dwóch podanych liczb całkowitych.
+
+//    int a, b;
+//
+//    printf("Podaj pierwsza liczbe calkowita: ");
+//    scanf("%d", &a);
+//
+//    printf("Podaj druga liczbe calkowita: ");
+//    scanf("%d", &b);
+//
+//    int roznica = a - b;
+//    int wynik = roznica * roznica;
+//
+//    printf("Wynik: %d", wynik);
+
+//    Zad 9
+//    Napisz program, który oblicza iloraz sumy dwóch liczb całkowitych przez ich różnicę.
+
+//    int a, b;
+//
+//    printf("Podaj pierwsza liczbe calkowita: ");
+//    scanf("%d", &a);
+//
+//    printf("Podaj druga liczbe calkowita: ");
+//    scanf("%d", &b);
+//
+//    double suma = a + b;
+//    double roznica = a - b;
+//    double wynik = suma / roznica;
+//    printf("Wynik: %0.2lf", wynik);
+
+//    Zad 10
+//    Stwórz program, który oblicza sumę trzech kolejnych liczb całkowitych, zaczynając od podanej liczby całkowitej.
+
+//    int a;
+//
+//    printf("Podaj poczatkowa liczbe calkowita: ");
+//    scanf("%d", &a);
+//
+//    int suma = a + (a + 1) + (a + 2);
+//
+//    printf("Wynik: %d", suma);
+
+//    Zad 11
+//    Znajdź średnią arytmetyczną trzech liczb zmiennoprzecinkowych.
+
+//    double a, b, c;
+//
+//    printf("Podaj trzy liczby zmiennoprzecinkowe oddzielone spacja: ");
+//    scanf("%lf %lf %lf", &a, &b, &c);
+//
+//    double srednia = (a + b + c) / 3;
+//
+//    printf("Srednia arytmetyczna wynosi: %.2lf", srednia);
+
+//    Zad 12
+//    Oblicz wartość wyrażenia
+
+//    double a, b, c;
+//
+//    printf("Podaj trzy rozne od zera liczby zmiennoprzecinkowe: ");
+//    scanf("%lf %lf %lf", &a, &b, &c);
+//
+//    double wynik = 1 / ((1 / a) + (1 / b) + (1 / c));
+//    printf("Wynik: %0.2lf", wynik);
+
+//    Zad 13
+//    Wylicz wartość
+
+//    double x;
+//
+//    printf("Podaj wartosc x: ");
+//    scanf("%lf", &x);
+//
+//    double wynik = 1 + x;
+//
+//    printf("Przyblizona wartosc e^x dla x = %lf wynosi: %.6lf", x, wynik);
+
+//    Zad 14
+//    Oblicz pole trójkąta
+
+//    double a, b, C;
+//    double PI = 3.14;
+//
+//    printf("Podaj bok a, b oraz stopnie miedzy nimi: ");
+//    scanf("%lf %lf %lf", &a, &b, &C);
+//    double radiany = C * PI / 180;
+//
+//    double pole = 0.5 * a * b * radiany;
+//
+//    printf("Pole: %0.4lf", pole);
+
+
+//    Zad 15
+//    Oblicz
+
+//    double x;
+//
+//    printf("Podaj wartosc x: ");
+//    scanf("%lf", &x);
+//
+//    double wynik = 1 + (x / 3);
+//
+//    printf("Przyblizona wartosc : %.4lf", wynik);
 
     return 0;
 }
