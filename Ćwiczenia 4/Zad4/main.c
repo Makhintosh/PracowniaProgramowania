93763c079a443115520850a67caf3f75072387f5
Makhintosh <82097837+Makhintosh@users.noreply.github.com>
2026-03-06 09:27:24 +0100

    styrta sie pali

---
+++ Ćwiczenia 4/Zad4/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//    int a = 6, b = 3;
+//    printf("%d\n", a & b);
+//    printf("%d\n", a | b);
+//    printf("%d\n", a ^ b);
+//    printf("%d\n", ~a);
+//    printf("%d\n", a << 2);
+//    printf("%d", a >> 2);
+
+//    Zad 1
+//    Zamieñ wartoœci dwóch zmiennych ca³kowitych bez u¿ycia dodatkowej zmiennej.
+
+//    int a = 3, b = 4; // 0b0011 = 3 0b0100
+//    printf("Przed zmiana: a = %d, b = %d\n", a, b);
+//
+//    a = a ^ b;
+//    b = a ^ b;
+//    a = a ^ b;
+//
+//    printf("Po zmianie: a = %d, b = %d", a, b);
+
+//    Zad 2
+//    SprawdŸ, czy liczba ca³kowita jest parzysta czy nieparzysta za pomoc¹ operatorów bitowych.
+
+//    int liczba;
+//    printf("Podaj liczbe: ");
+//    scanf("%d", &liczba);
+//
+//    if ((liczba & 1) == 0){
+//        printf("Liczba parzysta");
+//    }else{
+//        printf("Liczba nieparzysta");
+//    }
+
+//    Zad 3
+//    Wyznacz wartoœæ bitu na okreœlonej pozycji w liczbie ca³kowitej.
+
+
+
+//    Zad 4
+//    Zeruj wartoœæ bitu na okreœlonej pozycji w liczbie ca³kowitej.
+
+//    Zad 5
+//    Odwróæ wartoœæ wszystkich bitów w liczbie ca³kowitej.
+
+//    Zad 6
+//    Przesuñ bity liczby ca³kowitej o okreœlon¹ liczbê pozycji w lewo.
+
+//    Zad 7
+//    Przesuñ bity liczby ca³kowitej o okreœlon¹ liczbê pozycji w prawo.
+
+//    Zad 8
+//    Wyznacz “XOR” dwóch liczb ca³kowitych.
+    return 0;
+}
