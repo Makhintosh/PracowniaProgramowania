b0cf291ebf285c54c2ef6c5be937fbec6267c9a5
Makhintosh <igoruszat@gmail.com>
2026-03-11 16:33:56 +0100

    <3

--- Ćwiczenia 4/Zad4/main.c
+++ Ćwiczenia 4/Zad4/main.c
@@ -39,21 +39,62 @@ int main()
 //    Zad 3
 //    Wyznacz wartoœæ bitu na okreœlonej pozycji w liczbie ca³kowitej.
 
-
+//    int liczba = 13;
+//    int pozycja = 2;
+//
+//    int wartosc_bitu = (liczba >> pozycja) & 1;
+//
+//    printf("Wartosc bitu na pozycji %d w liczbie %d to: %d\n", pozycja, liczba, wartosc_bitu);
 
 //    Zad 4
 //    Zeruj wartoœæ bitu na okreœlonej pozycji w liczbie ca³kowitej.
 
+//    int liczba = 15;
+//    int pozycja = 1;
+//
+//    printf("Przed: %d\n", liczba);
+//
+//    liczba = liczba & ~(1 << pozycja);
+//
+//    printf("Po wyzerowaniu bitu na pozycji %d: %d\n", pozycja, liczba);
+
 //    Zad 5
 //    Odwróæ wartoœæ wszystkich bitów w liczbie ca³kowitej.
+
+//    int liczba = 5;
+//    int odwrocona = ~liczba;
+//
+//    printf("Odwrocenie wszystkich bitow dla %d daje: %d\n", liczba, odwrocona);
 
 //    Zad 6
 //    Przesuñ bity liczby ca³kowitej o okreœlon¹ liczbê pozycji w lewo.
 
+//    int liczba = 3;
+//    int przesuniecie = 2;
+//
+//    int wynik = liczba << przesuniecie;
+//
+//    printf("%d przesuniete o %d pozycje w lewo to: %d\n", liczba, przesuniecie, wynik);
+
 //    Zad 7
 //    Przesuñ bity liczby ca³kowitej o okreœlon¹ liczbê pozycji w prawo.
 
+//    int liczba = 20;
+//    int przesuniecie = 2;
+//
+//    int wynik = liczba >> przesuniecie;
+//
+//    printf("%d przesuniete o %d pozycje w prawo to: %d\n", liczba, przesuniecie, wynik);
+
 //    Zad 8
 //    Wyznacz “XOR” dwóch liczb ca³kowitych.
+//
+//    int a = 12;
+//    int b = 10;
+//
+//    int wynik = a ^ b;
+//
+//    printf("Wynik dzialania %d XOR %d wynosi: %d\n", a, b, wynik);
+
     return 0;
 }
